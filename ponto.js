58f8f8068eed8aaf7cad475b3eec2665ef8754a1
Guilherme Travinski Ferreira <guitrafer@gmail.com>
2021-06-07 13:57:07 +0000

    1

--- ponto.js
+++ ponto.js
@@ -3,3 +3,9 @@ form = document.getElementById("tblCartao")
 tabela = form.getElementsByTagName("tr")
 
 tabela[0].innerText
+
+table = document.getElementsByClassName("col-sm-6")
+table[0].innerText
+
+aux = a.slice(0, a.search("\n"))
+a = a.slice(a.search("\n") + 1, a.length)
